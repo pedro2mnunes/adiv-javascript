4441faed6eda44c1e667a129c4d51ddd3462571e
Breno Salles <git@brenosalles.com>
2020-12-15 10:15:21 +0000

    change: Added "return" explanation

--- t/t4/js/script.js
+++ t/t4/js/script.js
@@ -60,3 +60,36 @@ setTimeout(() => {
 window.setInterval(() => {
   console.log("5 segundos");
 }, 5000);
+
+function sum(n1, n2) {
+  // return é do tipo "number"
+  if (typeof n1 != "number") {
+    return -1;
+  }
+  if (typeof n2 != "number") {
+    return -1;
+  }
+  const resultado = n1 + n2;
+  return resultado;
+}
+
+const result = sum(5, 7);
+console.log(`O resultado é: ${result}`);
+
+function retornaString() {
+  // Retorna tipo "string"
+  return "Nuno";
+}
+const resultadoNuno = retornaString();
+console.log(resultadoNuno);
+
+function retornaVoid(param1) {
+  if (typeof param1 == "number") {
+    console.log("Tipo number")
+    return;
+  }
+  console.log(param1);
+}
+
+retornaVoid(1);
+retornaVoid("Hellow ");
